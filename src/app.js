4a490579e9e4ee52a5a6de94c4efdf03e0cece0c
Leo Turzi <leogturzi@gmail.com>
2023-01-24 17:13:04 -0700

    Implement Webpack

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js';
-import {
-    getDatabase,
-    ref,
-    set,
-} from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, push } from 'firebase/database';
+import moment from 'moment';
+import 'moment-timezone';
 
 const firebaseConfig = {
     apiKey: 'AIzaSyCMvxkc0UMVK4p7Ca7oTI7O4TaC2Sn3QbA',
@@ -27,19 +25,37 @@ const form = document.querySelector('#cargar_movimiento');
 const hsTrabajoInicio = document.querySelector('#hora_trabajo_inicio');
 const aeronaveMatricula = document.querySelector('#aeronave_matricula');
 const hsEntrega = document.querySelector('#hora_entrega');
+const now = document.querySelector('#now');
 const btn = document.querySelector('#submit');
+
+now.addEventListener('change', (e) => {
+    if (now.checked) {
+        hsEntrega.value = moment
+            .tz('America/Argentina/Buenos_Aires')
+            .format('hh:mm');
+        hsEntrega.disabled = true;
+    } else {
+        hsEntrega.disabled = false;
+    }
+});
 
 form.addEventListener('submit', (e) => {
     console.log('pase');
     e.preventDefault();
+    console.log(hsEntrega.value);
     cargarMovimiento(hsTrabajoInicio, aeronaveMatricula, hsEntrega);
     console.log('pase');
 });
 
 function cargarMovimiento(hsTrabajoInicio, aeronaveMatricula, hsEntrega) {
-    set(ref(db, 'movimientos/' + 1), {
+    const movimientos = ref(db, 'Movimientos');
+    const nuevoMovimiento = push(movimientos);
+    set(nuevoMovimiento, {
         hsTrabajoInicio: hsTrabajoInicio.value,
         aeronaveMatricula: aeronaveMatricula.value,
         hsEntrega: hsEntrega.value,
+        createdAt: moment
+            .tz('America/Argentina/Buenos_Aires')
+            .format('YYYY-MM-DD h:mm:ss a'),
     });
 }
