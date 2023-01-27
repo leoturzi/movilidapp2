d85c0661479e596309103b5adb89798744ad3530
Leo Turzi <leogturzi@gmail.com>
2023-01-27 16:23:46 -0700

    Migrating to React

--- src/app.js
+++ src/app.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <form id='cargar_movimiento'>
+            <label for='hora_trabajo_inicio'>
+                Horas de Trabajo Inicio
+                <input
+                    type='text'
+                    name='hora_trabajo_inicio'
+                    id='hora_trabajo_inicio'
+                    value='12314'
+                />{' '}
+            </label>
+            <br />
+            <label for='aeronave_matricula'>
+                Aeronave
+                <input
+                    type='text'
+                    name='aeronave_matricula'
+                    id='aeronave_matricula'
+                    value='tc61'
+                />{' '}
+            </label>
+            <br />
+            <label for='hora_entrega'>
+                Hora de Entrega
+                <input
+                    type='time'
+                    name='hora_entrega'
+                    id='hora_entrega'
+                    value='12:00'
+                />
+            </label>
+            <br />
+            <label for='now'>
+                Ahora <input type='checkbox' name='now' id='now' />{' '}
+            </label>
+            <br />
+            <button id='submit' type='submit'>
+                Cargar
+            </button>
+        </form>
+    );
 }
 
 export default App;
