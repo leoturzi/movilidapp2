224fdc8b539d1aa9cd70311ec02b37ff2cd66480
Leo Turzi <leogturzi@gmail.com>
2023-02-02 02:36:45 -0700

    Create Equipos component to improve navigation

--- src/firebase.js
+++ src/firebase.js
@@ -86,13 +86,30 @@ export async function getLastMovimientoEquipo(equipodId) {
         orderByChild('equipo'),
         equalTo(equipodId)
     );
+    if (queryMovimientos) {
+        try {
+            const snapshot = await get(queryMovimientos);
+            if (snapshot.exists()) {
+                const id = Object.keys(snapshot.val())[0];
+                const data = snapshot.val()[id];
+                return { data, id };
+            } else {
+                console.log('No data available');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return { data: 'no hay movimientos', id: 1 };
+}
+
+export async function getEquipos() {
+    const queryEquipos = query(ref(db, 'Equipos'));
 
     try {
-        const snapshot = await get(queryMovimientos);
+        const snapshot = await get(queryEquipos);
         if (snapshot.exists()) {
-            const id = Object.keys(snapshot.val())[0];
-            const data = snapshot.val()[id];
-            return { data, id };
+            return snapshot.val();
         } else {
             console.log('No data available');
         }
