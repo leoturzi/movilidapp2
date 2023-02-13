5141af2ea2ac9b7abca46bf92a4a515b9912a8c3
Leo Turzi <35748832+leoturzi@users.noreply.github.com>
2023-02-12 23:46:26 -0300

    Develop (#16)
    * Fix behavior on user settings
    
    * Remove console logs
    
    * Improve login style

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../../firebase';
 
+import {
+    Typography,
+    Container,
+    TextField,
+    Button,
+    Grid,
+    Stack,
+} from '@mui/material';
+
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -37,31 +46,54 @@ function Login() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='username'>
-                Usuario
-                <input
-                    type='text'
-                    name='username'
-                    onChange={handleChange}
-                    value={username}
-                />
-                {error?.errorCode === 2 && <p>{error.errorMessage}</p>}
-            </label>
-            <br />
-            <label htmlFor='password'>
-                Contraseña
-                <input
-                    type='password'
-                    name='password'
-                    onChange={handleChange}
-                    value={password}
-                />
-                {error?.errorCode === 1 && <p>{error.errorMessage}</p>}
-            </label>
-            <br />
-            <button type='submit'>Loguearse</button>
-        </form>
+        <Container>
+            <form onSubmit={handleSubmit}>
+                <Stack
+                    spacing={2}
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        padding: '1rem',
+                        border: '1px solid #ccc',
+                        boxShadow: '0 0 8px #ccc',
+                        margin: '4rem 1rem 0 1rem',
+                        borderRadius: '5px',
+                    }}
+                >
+                    <Typography variant='h6'>Logueate</Typography>
+                    <TextField
+                        size='small'
+                        error={error?.errorCode === 2 ? true : false}
+                        name='username'
+                        type='text'
+                        id='Usuario'
+                        label='Usuario'
+                        onChange={handleChange}
+                        value={username}
+                        helperText={
+                            error?.errorCode === 2 ? error.errorMessage : ''
+                        }
+                    />
+                    <TextField
+                        size='small'
+                        error={error?.errorCode === 1 ? true : false}
+                        name='password'
+                        id='Password'
+                        label='Password'
+                        type='password'
+                        onChange={handleChange}
+                        value={password}
+                        helperText={
+                            error?.errorCode === 1 ? error.errorMessage : ''
+                        }
+                    />
+                    <Button type='submit' size='large'>
+                        Loguearse
+                    </Button>
+                </Stack>
+            </form>
+        </Container>
     );
 }
 
